c27e922ef83102086e4c0a28cfffc228b8352238
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-04-07 20:21:25 +0200

    Remove Mui Grid from ArticlePreviewDensedLayout

--- src/article/preview/ArticlePreviewDensedLayout.tsx
+++ src/article/preview/ArticlePreviewDensedLayout.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Grid } from '@material-ui/core';
 import { Box } from 'shared/general/layout/Box';
 import { Edit, Place, FiberManualRecord } from '@material-ui/icons';
 import { format, isBefore } from 'date-fns';
@@ -13,11 +12,12 @@ import { BackgroundImg } from 'react-cloudimage-responsive';
 import { Button } from 'shared/general/button/Button';
 import { useServerData } from 'shared/ServerDataContext';
 import { AuthorAvatarsList } from 'article/authorAvatarsList/AuthorAvatarsList';
-import ToggleArticlePinMutation from 'api/mutation/ToggleArticlePin.graphql';
 import clsx from 'clsx';
 import Link from 'next/link';
 
 import styles from './ArticlePreviewDensedLayout.module.scss';
+
+import ToggleArticlePinMutation from 'api/mutation/ToggleArticlePin.graphql';
 
 interface ArticlePreviewProps {
     article: ArticleModel;
@@ -64,113 +64,104 @@ export const ArticlePreviewDensedLayout = React.memo<ArticlePreviewProps>(
                 })}
                 data-testid="ArticlePreviewDensedLayout"
             >
-                <Grid
-                    container
-                    style={{
-                        position: 'relative',
-                        display: 'flex',
-                        minHeight: 60,
-                    }}
-                >
+                <>
                     {article.previewImageFile && (
-                        <Grid item xs={2} style={{ position: 'relative' }}>
+                        <div className={styles.previewImageWrapper}>
                             {maybeLinked(
                                 <BackgroundImg
-                                    height={'100%'}
-                                    src={File.getFileRemoteLocation(
-                                        baseUrl,
-                                        article.previewImageFile
-                                    )}
-                                    className={styles.articlePreviewImage}
-                                    params="func=crop&gravity=auto"
+                                height={'100%'}
+                                src={File.getFileRemoteLocation(
+                                    baseUrl,
+                                    article.previewImageFile
+                                )}
+                                className={styles.articlePreviewImage}
+                                params="func=crop&gravity=auto"
                                 />
                             )}
-                        </Grid>
-                    )}
-                    <Grid item xs>
-                        <div className={styles.boxContent}>
-                            <h3 className={styles.articleTitle}>
-                                {!isEmbedded &&
-                                    currentUser &&
-                                    currentUser.lastSeen &&
-                                    isBefore(
-                                        new Date(currentUser.lastSeen),
-                                        new Date(article.updatedAt)
-                                    ) && (
-                                        <FiberManualRecord
-                                            color={'secondary'}
-                                            fontSize={'small'}
-                                        />
-                                    )}
-                                {maybeLinked(article.title)}
-                            </h3>
-                            <span
-                                className={clsx(styles.previewText, {
-                                    [styles.previewTextLimitedHeight]:
-                                        limitedHeight,
-                                })}
-                            >
-                                {article.preview}
-                            </span>
                         </div>
-                    </Grid>
-                    <Grid item xs={2} className={styles.meta}>
-                        <span className={clsx(styles.subtitle)}>
-                            {format(new Date(article.updatedAt), 'P', {
-                                locale: de,
-                            }) + ' '}
-                        </span>
-                        <span className={clsx(styles.subtitle)}>
-                            {article.users && article.users.length > 0 && (
-                                <>
-                                    &nbsp;
-                                    <AuthorAvatarsList users={article.users} />
-                                    &nbsp;
-                                </>
-                            )}
-                        </span>
-                    </Grid>
+                    )}
+            <div className={styles.boxContent}>
+            <h3 className={styles.articleTitle}>
+            {!isEmbedded &&
+                currentUser &&
+                currentUser.lastSeen &&
+                isBefore(
+                    new Date(currentUser.lastSeen),
+                    new Date(article.updatedAt)
+                ) && (
+                    <FiberManualRecord
+                    color={'secondary'}
+                    fontSize={'small'}
+                    />
+                )}
+            {maybeLinked(article.title)}
+            </h3>
+            <span
+            className={clsx(styles.previewText, {
+                [styles.previewTextLimitedHeight]:
+                limitedHeight,
+            })}
+                            >
+    {article.preview}
+    </span>
+    </div>
+    <div className={styles.meta}>
+    <span className={clsx(styles.subtitle)}>
+    {format(new Date(article.updatedAt), 'P', {
+        locale: de,
+    }) + ' '}
+            </span>
+            <span className={clsx(styles.subtitle)}>
+            {article.users && article.users.length > 0 && (
+                <>
+                &nbsp;
+                <AuthorAvatarsList users={article.users} />
+                &nbsp;
+                </>
+            )}
+            </span>
+            </div>
                     {!isMobile &&
                         ((!disableEdit &&
                             User.canEditArticle(currentUser, article)) ||
                             (!disablePin && User.isAdmin(currentUser))) && (
-                            <Grid item xs={1} className={styles.editSection}>
+                                <div className={styles.editSection}>
                                 {!disableEdit &&
                                     User.canEditArticle(
                                         currentUser,
                                         article
                                     ) && (
                                         <Link
-                                            href={Article.getPath(article, {
-                                                edit: true,
-                                            })}
-                                            passHref
-                                        >
-                                            <Button
-                                                small
-                                                aria-label="Edit"
-                                                className={clsx(
-                                                    styles.editButton,
-                                                    'edit-button'
-                                                )}
-                                                icon={<Edit />}
-                                            />
+                                        href={Article.getPath(article, {
+                                            edit: true,
+                                        })}
+                                        passHref
+                                    >
+                                        <Button
+                                        small
+                                        aria-label="Edit"
+                                        className={clsx(
+                                            styles.editButton,
+                                            'edit-button'
+                                        )}
+                                        icon={<Edit />}
+                                        />
                                         </Link>
                                     )}
                                 {!disablePin && User.isAdmin(currentUser) && (
                                     <Button
-                                        small
-                                        aria-label="Pin"
-                                        className={clsx(styles.pinButton, {
-                                            active: article.isPinnedToTop,
-                                        })}
-                                        onClick={() => toggleArticlePin()}
-                                        icon={<Place />}
+                                    small
+                                    aria-label="Pin"
+                                    className={clsx(styles.pinButton, {
+                                        active: article.isPinnedToTop,
+                                    })}
+                                    onClick={() => toggleArticlePin()}
+                                    icon={<Place />}
                                     />
                                 )}
-                            </Grid>
+                                </div>
                         )}
-                </Grid>
+                </>
             </Box>
         );
     }
