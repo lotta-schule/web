3a99f1cbb068353d121c064b95329d610a9e214e
Christopher Bill <cb.billy.94@gmail.com>
2022-05-04 20:13:40 +0200

    replaced MUI tabs by lotta tabbar component

--- src/shared/general/tabs/Tabbar.tsx
+++ src/shared/general/tabs/Tabbar.tsx
@@ -2,19 +2,34 @@ import * as React from 'react';
 import clsx from 'clsx';
 
 import styles from './Tab.module.scss';
+import { Tab, TabProps } from './Tab';
 
 export type TabbarProps = {
     className?: string;
+    value?: string | number;
+    onChange?: (value: string | number) => void;
+    children?: any;
 };
 
 export const Tabbar: React.FC<TabbarProps> = ({
     className,
+    onChange,
+    value,
     children,
-    ...props
 }) => {
     return (
-        <div className={clsx(styles.tabbar, className)} {...props}>
-            {children}
+        <div className={clsx(styles.tabbar, className)}>
+            {React.Children.map(
+                children as any,
+                (child: React.ReactElement<TabProps>, i) =>
+                    React.cloneElement(child as any, {
+                        key: i,
+                        onClick: () => {
+                            onChange?.(child.props.value as string);
+                        },
+                        selected: value === child.props.value,
+                    })
+            )}
         </div>
     );
 };
--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -11,17 +11,20 @@ export default {
     argTypes: {},
 } as Meta;
 
-const Template: Story = (args) => <Tabbar {...args} />;
+const Template: Story = (args) => {
+    const [value, setValue] = React.useState('0');
+    return (
+        <Tabbar value={value} onChange={(value) => setValue(value as string)}>
+            <Tab value={'0'}>Tab1 bla bla bla</Tab>
+            <Tab value={'1'}>Tab2</Tab>
+            <Tab value={'2'}>Tab3</Tab>
+            <Tab value={'3'}>Tab4 dingsi bumso</Tab>
+            <Tab value={'4'}>Tab5</Tab>
+        </Tabbar>
+    );
+};
 
 export const Default = Template.bind({});
 Default.args = {
-    children: (
-        <>
-            <Tab>Tab1</Tab>
-            <Tab>Tab2</Tab>
-            <Tab>Tab3</Tab>
-            <Tab>Tab4</Tab>
-            <Tab>Tab5</Tab>
-        </>
-    ),
+    value: 2,
 };
