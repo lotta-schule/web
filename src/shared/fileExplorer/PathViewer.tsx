8e32c7e9731eb65f3d94dc76143b7bcc7a63b038
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-06-28 19:32:20 +0200

    FileExplorer: PathViewer: Add functionality

--- src/shared/fileExplorer/PathViewer.tsx
+++ src/shared/fileExplorer/PathViewer.tsx
@@ -1,39 +1,42 @@
 import * as React from 'react';
 import { HomeOutlined } from '@material-ui/icons';
+import { defaultState } from './context/FileExplorerContext';
 
 import styles from './PathViewer.module.scss';
 
-export interface PathViewerProps {}
+type Path = typeof defaultState['currentPath'];
 
-export const PathViewer = React.memo<PathViewerProps>(() => {
+export interface PathViewerProps {
+    path: Path;
+    onChange: (path: Path) => void;
+}
+
+export const PathViewer = React.memo(({ path, onChange }: PathViewerProps) => {
     return (
         <div className={styles.root}>
-            <a
-                onClick={(e) => {
-                    e.preventDefault();
-                    alert('HOME');
-                }}
-            >
-                <HomeOutlined />
-            </a>
-            <div className={styles.paths}>
-                /
-                <a
-                    onClick={(_e) => {
-                        alert('click');
-                    }}
-                >
-                    Test1
-                </a>
-                /
-                <a
-                    onClick={(_e) => {
-                        alert('click');
-                    }}
-                >
-                    Test2
-                </a>
-            </div>
+            {path.map((component, i) => (
+                <React.Fragment key={component.id ?? 'root'}>
+                    {i !== 0 && <>/</>}
+                    <a
+                        href={'#'}
+                        aria-label={
+                            'name' in component
+                                ? component.name
+                                : 'Wurzelverzeichnis'
+                        }
+                        onClick={(e) => {
+                            e.preventDefault();
+                            onChange(path.slice(0, i + 1));
+                        }}
+                    >
+                        {'name' in component ? (
+                            component.name
+                        ) : (
+                            <HomeOutlined />
+                        )}
+                    </a>
+                </React.Fragment>
+            ))}
         </div>
     );
 });
